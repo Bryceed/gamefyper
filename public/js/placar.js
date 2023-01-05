3502311624ab92f13aef23f44400b637963766c3
WellingtonESX <wellington.tvd@outlook.com.br>
2023-01-05 18:13:27 -0300

    Initial commit

---
+++ public/js/placar.js
@@ -0,0 +1,46 @@
+function inserePlacar() {
+    var corpoTabela = $(".placar").find("tbody");
+    // get user from cookie, or ask for it
+    var usuario = getUsuario();
+    var numPalavras = $("#contador-palavras").text();
+
+    var linha = novaLinha(usuario, numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
+
+    corpoTabela.append(linha);
+}
+
+function getUsuario() {
+    // from cookie
+    if (usuario) {
+        return usuario;
+    } else {
+        // ask for it
+        return prompt("Qual o seu nome?");
+    }
+}
+
+function novaLinha(usuario, palavras) {
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(palavras);
+    var colunaRemover = $("<td>");
+
+    var link = $("<a>").addClass("botao-remover").attr("href", "#");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone);
+
+    colunaRemover.append(link);
+
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+function removeLinha() {
+    event.preventDefault();
+    $(this).parent().parent().remove();
+}
